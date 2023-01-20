c6397ab624fda2d43149e982ef1ca88365e50db0
unknown <mollorefilwe68@gmail.com>
2023-01-21 01:50:46 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+import Navbar from './components/Navbar';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Booknow from './pages/Booknow';
+import Payment from './pages/Payments';
+import Gallery from './pages/gallery';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+
+          <Route exact path="/" element={<Home />}/>
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/rooms/:id" component={SingleRoom} />
+          <Route path="/rooms/" component={Rooms}/>
+          <Route path="/booknow/:id" component={Booknow} />
+          <Route path='/payments' component={Payment}/>
+          <Route path='/gallery' component={Gallery}/>
+
+          <Route component={Error}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
